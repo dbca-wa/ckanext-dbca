7df84dfee5b75d946f263ae07360c31816ba40f5
Awang <awang.setyawan@salsa.digital>
2023-12-20 11:02:45 +0700

    [BIO-6387-6392] added darwin core and embargo field (#9)
    * [BIO-6387] added darwin core to resource formats
    
    * [BIO-6392] added embargo date, validator and its templates
    
    * [BIO-6392] added command to publish and send email notification
    
    * [BIO-6392] added email notification
    
    * [BIO-6392] remove unused implementation, refactor variable names, added comments
    
    * Removed organisation form snippet and revert to default dataset_organisation preset
    Added missing Environment choice to theme
    Disabled dbca_dev.css theme
    
    * [BIO-6392] refactored the code, set date to Perth instead of UTC, removed presets
    
    * [BIO-6392] removed double quotes and use local_now for comparison
    
    ---------
    
    Co-authored-by: MarkCalvert <37602611+MarkCalvert@users.noreply.github.com>

--- ckanext/dbca/logic/action.py
+++ ckanext/dbca/logic/action.py
@@ -1,3 +1,7 @@
+import datetime
+import pytz
+
+import ckan.model as model
 import ckan.plugins.toolkit as tk
 import ckanext.dbca.logic.schema as schema
 
@@ -19,7 +23,47 @@ def dbca_get_sum(context, data_dict):
     }
 
 
+@tk.side_effect_free
+def dbca_get_packages_to_be_published_or_notified(context, data_dict):
+    '''
+    Get all packages that need to be publshed or need to be notified.
+    Notification will be sent for package that will be published in the next 7 days.
+    '''
+    # UTC now converted to Perth timezone
+    aus_tz = tk.h.get_display_timezone()
+    utc_now = pytz.utc.localize(datetime.datetime.utcnow())
+    local_now = utc_now.astimezone(aus_tz).date()
+
+    # Calculate the date 7 days from now
+    date_in_7_days = local_now + datetime.timedelta(days=7)
+
+    # Query all active private packages with an embargo date
+    packages = model.Session.query(model.Package, model.PackageExtra) \
+        .join(model.PackageExtra, model.Package.id == model.PackageExtra.package_id) \
+        .filter(model.PackageExtra.key == 'embargo') \
+        .filter(model.Package.state == 'active') \
+        .filter(model.Package.private) \
+        .all()
+
+    # Lists to hold packages for publishing and notifying
+    packages_to_publish = []
+    packages_to_notify = []
+
+    # Check each dataset for action
+    for package, package_extra in packages:
+        # Convert embargo date string to date object
+        embargo_date = datetime.datetime.strptime(package_extra.value, '%Y-%m-%d').date()
+
+        if embargo_date <= local_now:
+            packages_to_publish.append((package, package_extra))
+        elif embargo_date == date_in_7_days:
+            packages_to_notify.append((package, package_extra))
+
+    return {"to_publish": packages_to_publish, "to_notify": packages_to_notify}
+
+
 def get_actions():
     return {
         'dbca_get_sum': dbca_get_sum,
+        'dbca_get_packages_to_be_published_or_notified': dbca_get_packages_to_be_published_or_notified
     }
